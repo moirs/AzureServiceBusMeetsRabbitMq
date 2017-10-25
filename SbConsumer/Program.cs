96ef12816361aa8282c2af05f79b84bd2312abeb
moirs <scot@microstormsoftware.com>
2017-10-25 14:22:23 +0100

    Add project files.

---
+++ SbConsumer/Program.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Configuration;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace SbConsumer
+{
+    class Program
+    {
+        private static ConnectionFactory _connectionFactory;
+        private static IConnection _connection;
+        private static IModel _model;
+        private const string QueueName = "TestMessagingQueue";
+
+        static void Main(string[] args)
+        {
+            var runAzureSolution = bool.Parse(ConfigurationManager.AppSettings.Get("RunAzureSolution"));
+            var displayInfo = runAzureSolution ? "*** AZURE CONSUMER ***" : "*** RABBITMQ CONSUMER ***";
+
+            Console.WriteLine(PrintCharacters('*', displayInfo.Length));
+            Console.WriteLine(displayInfo);
+            Console.WriteLine(PrintCharacters('*', displayInfo.Length));
+            Console.WriteLine();
+
+            if (runAzureSolution)
+            {
+                // Azure
+                ConsumeAzureServiceBusMessage();
+            }
+            else
+            {
+                // RabbitMq
+                CreateRabbitMessageQueue();
+                ConsumeRabbitMqMessage();
+            }            
+        }
+
+        private static void ConsumeAzureServiceBusMessage()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["Microsoft.ServiceBus.ConnectionString"].ConnectionString;
+            var client = QueueClient.CreateFromConnectionString(connectionString);
+
+            client.OnMessage(message =>
+            {
+                var msgBody = message.GetBody<string>();
+                Console.WriteLine($"Processing {message.MessageId} with body {msgBody}");
+                if (msgBody.Equals("quit"))
+                {
+                    Console.WriteLine("Exiting application...");
+                    Environment.Exit(0);
+                }
+            });
+
+            Console.ReadLine();
+        }
+
+        private static void ConsumeRabbitMqMessage()
+        {
+            var consumer = new EventingBasicConsumer(_model);
+            consumer.Received += (m, ea) =>
+            {
+                Console.WriteLine($"Processing {ea.BasicProperties.MessageId} with body {System.Text.Encoding.Default.GetString(ea.Body)}");
+            };
+
+            _model.BasicConsume(QueueName, true, consumer);
+
+            Console.ReadLine();
+        }
+
+        private static void CreateRabbitMessageQueue()
+        {
+            // docker command to run hosted rabbitmq in container: docker run --rm -it --hostname rabbit-hostname --name rabbit-name -p 5672:5672 -p 8080:15672 rabbitmq:3-management
+            _connectionFactory = new ConnectionFactory { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest", VirtualHost = "/" };
+            _connection = _connectionFactory.CreateConnection();
+            _model = _connection.CreateModel();
+            _model.QueueDeclare(QueueName, true, false, false, null);
+        }
+
+        private static string PrintCharacters(char characterToPrint, int noOfCharactersToPrint)
+        {
+            var output = string.Empty;
+
+            for (int i = 0; i < noOfCharactersToPrint; i++)
+            {
+                output += characterToPrint;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ SbProducer/Program.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using RabbitMQ.Client;
+using System;
+using System.Configuration;
+
+namespace SbProducer
+{
+    class Program
+    {
+        private static ConnectionFactory _connectionFactory;
+        private static IConnection _connection;
+        private static IModel _model;
+        private const string QueueName = "TestMessagingQueue";
+
+        static void Main(string[] args)
+        {
+            var runAzureSolution = bool.Parse(ConfigurationManager.AppSettings.Get("RunAzureSolution"));
+            var displayInfo = runAzureSolution ? "*** AZURE PRODUCER ***" : "*** RABBITMQ PRODUCER ***";
+
+            Console.WriteLine(PrintCharacters('*', displayInfo.Length));
+            Console.WriteLine(displayInfo);
+            Console.WriteLine(PrintCharacters('*', displayInfo.Length));
+            Console.WriteLine();
+
+            if (bool.Parse(ConfigurationManager.AppSettings.Get("RunAzureSolution")))
+            {
+                // Azure
+                PublishAzureServiceBusMessage();
+            }
+            else
+            {
+                // RabbitMq
+                CreateRabbitMessageQueue();
+                PublishRabbitMqMessage();
+            }
+
+            Environment.Exit(0);
+        }
+
+        private static void PublishAzureServiceBusMessage()
+        {            
+            var connectionString = ConfigurationManager.ConnectionStrings["Microsoft.ServiceBus.ConnectionString"].ConnectionString;           
+            var client = QueueClient.CreateFromConnectionString(connectionString);
+
+            var lastKey = ' ';
+            var count = 1;
+
+            BrokeredMessage message;
+
+            while (lastKey != 'q')
+            {
+                var body = $"Message {count++}";
+                message = new BrokeredMessage(body);
+                client.Send(message);
+
+                Console.WriteLine($"Sent {message.MessageId}");
+                lastKey = Console.ReadKey().KeyChar;
+            }
+
+            message = new BrokeredMessage("quit");
+            client.Send(message);
+        }
+
+        private static void PublishRabbitMqMessage()
+        {            
+            var lastKey = ' ';
+            var count = 0;
+
+            while (lastKey != 'q')
+            {
+                var messageBodyBytes = System.Text.Encoding.UTF8.GetBytes($"Hello, world {++count}");
+                _model.BasicPublish("", QueueName, null, messageBodyBytes);
+
+                Console.WriteLine($"Sent {count}");
+                lastKey = Console.ReadKey().KeyChar;
+            }
+        }
+
+        private static void CreateRabbitMessageQueue()
+        {
+            // docker command to run hosted rabbitmq in container: docker run --rm -it --hostname rabbit-hostname --name rabbit-name -p 5672:5672 -p 8080:15672 rabbitmq:3-management
+            _connectionFactory = new ConnectionFactory{ HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest", VirtualHost = "/" };
+            _connection = _connectionFactory.CreateConnection();
+            _model = _connection.CreateModel();
+            _model.QueueDeclare(QueueName, true, false, false, null);
+        }
+
+        private static string PrintCharacters(char characterToPrint, int noOfCharactersToPrint)
+        {
+            var output = string.Empty;
+
+            for (int i = 0; i < noOfCharactersToPrint; i++)
+            {
+                output += characterToPrint;
+            }
+
+            return output;
+        }
+    }
+}
